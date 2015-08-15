cdca22ab16b78d7bc22decf9bd204b27df09b2e2
Ryan Gliever <ryangliever@gmail.com>
2015-08-15 14:46:13 -0700

    Added page title options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	# returns the title for a given page
+	def full_title(page_title = '')
+		base_title = "Sample App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
