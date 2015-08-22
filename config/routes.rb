59f61a9e4a607d7555aae0eb90762c4fc5a597a4
Ryan Gliever <ryangliever@gmail.com>
2015-08-22 00:36:05 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'   # creates help_path (/help) and help_url (www.example.com/help) 
   get 'about'   => 'static_pages#about'  # the same way as root
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  resources :users
 end
